602e335f072f1bce315e8fff18316f171bc455dd
Aliez <8287771+aliez-ren@users.noreply.github.com>
2026-03-15 18:43:32 +0900

    Update nvidia-nemotron-3-super.sh

--- nvidia-nemotron-3-super.sh
+++ nvidia-nemotron-3-super.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
+docker rm -f vllm-nvidia-nemotron-3-super
+
 docker run --device "nvidia.com/gpu=all" \
+  --name vllm-nvidia-nemotron-3-super \
+  -d \
   --shm-size 16g \
   -p 8000:8000 \
+  -e VLLM_NVFP4_GEMM_BACKEND=marlin \
+  -e VLLM_NVFP4_MOE_BACKEND=MARLIN \
   -v ~/.cache/huggingface:/root/.cache/huggingface \
   vllm/vllm-openai:v0.17.1-cu130 \
     --model nvidia/NVIDIA-Nemotron-3-Super-120B-A12B-NVFP4 \
     --async-scheduling \
-    --dtype auto \
-    --kv-cache-dtype fp8 \
     --tensor-parallel-size 1 \
     --pipeline-parallel-size 1 \
     --data-parallel-size 1 \
@@ -17,10 +21,11 @@ docker run --device "nvidia.com/gpu=all" \
     --attention-backend TRITON_ATTN \
     --gpu-memory-utilization 0.9 \
     --enable-chunked-prefill \
-    --max-num-seqs 512 \
     --host 0.0.0.0 \
     --port 8000 \
     --enable-auto-tool-choice \
     --tool-call-parser qwen3_coder \
     --reasoning-parser-plugin "/root/.cache/huggingface/hub/models--nvidia--NVIDIA-Nemotron-3-Super-120B-A12B-NVFP4/snapshots/167959da964ab08b30211f71e68f6670eaa87966/super_v3_reasoning_parser.py" \
     --reasoning-parser super_v3
+
+docker logs -f vllm-nvidia-nemotron-3-super
