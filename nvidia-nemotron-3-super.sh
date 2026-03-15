a7f514100fbd144590c29a8ef8559285686c368c
Aliez Ren <renzholy@gmail.com>
2026-03-15 14:54:21 +0900

    Add nvidia-nemotron-3-super.sh

---
+++ nvidia-nemotron-3-super.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+docker run --device "nvidia.com/gpu=all" \
+  --shm-size 16g \
+  -p 8000:8000 \
+  -v ~/.cache/huggingface:/root/.cache/huggingface \
+  vllm/vllm-openai:v0.17.1-cu130 \
+    --model nvidia/NVIDIA-Nemotron-3-Super-120B-A12B-NVFP4 \
+    --async-scheduling \
+    --dtype auto \
+    --kv-cache-dtype fp8 \
+    --tensor-parallel-size 1 \
+    --pipeline-parallel-size 1 \
+    --data-parallel-size 1 \
+    --swap-space 0 \
+    --trust-remote-code \
+    --attention-backend TRITON_ATTN \
+    --gpu-memory-utilization 0.9 \
+    --enable-chunked-prefill \
+    --max-num-seqs 512 \
+    --host 0.0.0.0 \
+    --port 8000 \
+    --enable-auto-tool-choice \
+    --tool-call-parser qwen3_coder \
+    --reasoning-parser-plugin "/root/.cache/huggingface/hub/models--nvidia--NVIDIA-Nemotron-3-Super-120B-A12B-NVFP4/snapshots/167959da964ab08b30211f71e68f6670eaa87966/super_v3_reasoning_parser.py" \
+    --reasoning-parser super_v3
