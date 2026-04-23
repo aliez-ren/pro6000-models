639d84042a6b31d7fbe2495f9fbd15238ec02ee2
Aliez Ren <renzholy@gmail.com>
2026-04-24 00:50:25 +0900

    update qwen

--- x4/qwen3-5-397b-a17b.sh
+++ x4/qwen3-5-397b-a17b.sh
@@ -1,30 +1,30 @@
 #!/usr/bin/env bash
 
-docker rm -f vllm-qwen3-5-397b-a17b-nvfp4
+docker rm -f qwen3-5-397b-a17b-nvfp4
 
-docker run --device "nvidia.com/gpu=all" \
-  --name vllm-qwen3-5-397b-a17b-nvfp4 \
-  -d \
-  --shm-size 16g \
-  -p 8000:8000 \
-  -e VLLM_USE_FLASHINFER_MOE_MXFP4_MXFP8_CUTLASS=1 \
-  -v /root/.cache/huggingface:/root/.cache/huggingface \
-  vllm/vllm-openai:cu130-nightly \
+docker run --device "nvidia.com/gpu=all"  \
+  --ipc=host --shm-size=8g --network=host \
+  -v ~/.cache/huggingface:/root/.cache/huggingface \
+  -v jit-cache:/cache/jit \
+  -e NCCL_P2P_LEVEL=SYS \
+  -e SGLANG_ENABLE_SPEC_V2=True \
+  voipmonitor/sglang:cu130 \
+  python3 -m sglang.launch_server \
     --model nvidia/Qwen3.5-397B-A17B-NVFP4 \
-    --async-scheduling \
-    --tensor-parallel-size 2 \
-    --pipeline-parallel-size 2 \
-    --data-parallel-size 1 \
+    --served-model-name Qwen3.5 \
+    --reasoning-parser qwen3 --tool-call-parser qwen3_coder \
+    --tensor-parallel-size 4 \
+    --quantization modelopt_fp4 --kv-cache-dtype fp8_e4m3 \
     --trust-remote-code \
-    --gpu-memory-utilization 0.95 \
-    --max-cudagraph-capture-size 64 \
-    --enable-chunked-prefill \
-    --enable-prefix-caching \
-    --language-model-only \
-    --host 0.0.0.0 \
-    --port 8000 \
-    --enable-auto-tool-choice \
-    --tool-call-parser qwen3_coder \
-    --reasoning-parser qwen3
+    --cuda-graph-max-bs 64 --max-running-requests 64 \
+    --mem-fraction-static 0.93 --chunked-prefill-size 16384 \
+    --host 0.0.0.0 --port 8000 \
+    --attention-backend flashinfer \
+    --fp4-gemm-backend b12x --moe-runner-backend b12x \
+    --enable-pcie-oneshot-allreduce \
+    --sleep-on-idle \
+    --mamba-scheduler-strategy extra_buffer \
+    --speculative-algo NEXTN --speculative-num-steps 4 \
+    --speculative-eagle-topk 1 --speculative-num-draft-tokens 6
 
-docker logs -f vllm-qwen3-5-397b-a17b-nvfp4
+docker logs -f qwen3-5-397b-a17b-nvfp4
