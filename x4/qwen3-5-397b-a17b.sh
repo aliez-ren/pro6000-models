deb5b535d69eb4ef0916c58a50e664333799aaa7
Aliez Ren <renzholy@gmail.com>
2026-04-25 15:32:39 +0900

    update

--- x4/qwen3-5-397b-a17b.sh
+++ x4/qwen3-5-397b-a17b.sh
@@ -14,22 +14,18 @@ docker run --device "nvidia.com/gpu=all"  \
     --model nvidia/Qwen3.5-397B-A17B-NVFP4 \
     --reasoning-parser qwen3 \
     --tool-call-parser qwen3_coder \
-    --tensor-parallel-size 4 \
+    --tensor-parallel-size 2 \
+    --pipeline-parallel-size 2 \
     --quantization modelopt_fp4 \
     --kv-cache-dtype fp8_e4m3 \
     --trust-remote-code \
     --cuda-graph-max-bs 64 \
     --max-running-requests 64 \
     --chunked-prefill-size 16384 \
-    --speculative-algo NEXTN \
-    --speculative-num-steps 5 \
-    --speculative-eagle-topk 1 \
-    --speculative-num-draft-tokens 6 \
     --mamba-scheduler-strategy extra_buffer \
     --mem-fraction-static 0.93 \
     --host 0.0.0.0 \
     --port 8000 \
-    --enable-pcie-oneshot-allreduce \
     --schedule-conservativeness 0.1 \
     --attention-backend flashinfer \
     --fp4-gemm-backend b12x \
